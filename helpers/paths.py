c1462d9ecdfe2e4381ab39a243caa0e45b49b2e6
matt <couperin100@yahoo.co.uk>
2018-03-26 10:53:03 +0100

    Added html reporting and paths helper file

---
+++ helpers/paths.py
@@ -0,0 +1,15 @@
+"""Common paths file.
+
+A single location to store path constants related to the project so that
+anywhere you need to get a path to a particular directory in the project you
+can import it from here."""
+
+from os import environ, path
+
+ROOT = environ['VIRTUAL_ENV']
+CONFIG_DIR = path.join(ROOT, 'config')
+ENVS = path.join(CONFIG_DIR, 'envs.yaml')
+RESULTS_DIR = path.join(ROOT, 'results')
+
+FULL_REPORT = path.join(RESULTS_DIR, 'report.html')
+
